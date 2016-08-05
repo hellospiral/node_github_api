b4c3a3c0aed76d271dbac8ca6871c7070dd92b6b
Matt Carlson <matthewthomascarlson@gmail.com>
2016-08-05 08:35:28 -0700

    create User object in user.js; get user-interface.js sucessfully making API request

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,1 +1,10 @@
-var apiKey = require('./../.env').apiKey;
+var User = require('./../js/user.js').userModule;
+
+$(document).ready(function() {
+  var currentUserObject = new User();
+  $('#userInfo').click(function() {
+    var name = $('#name').val();
+    $('#location').val("");
+    currentUserObject.getRepos(name);
+  });
+});
