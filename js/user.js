aed181a9344e61650209cfe575960ff6e2ef4f97
Matt Carlson <matthewthomascarlson@gmail.com>
2016-08-05 08:54:44 -0700

    alter the getRepos method of user.js to display repo names and descriptions in the DOM

--- js/user.js
+++ js/user.js
@@ -4,8 +4,21 @@ function User() {
 }
 
 User.prototype.getRepos = function(username) {
-  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+    var responseHtml = "";
+    var repos = response;
+    for (var i = 0; i < repos.length; i++) {
+      // console.log(response[i].name);
+      // console.log(response[i].description);
+      responseHtml += "<h3>" + repos[i].name + "</h3><br>";
+      if (repos[i].description === null) {
+        responseHtml += "<hr>";
+      } else {
+        responseHtml += "<h5>" + repos[i].description + "</h5><hr>";
+      }  
+    }
+    // console.log(response);
+    $(".showInfo").html(responseHtml);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
