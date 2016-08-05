3a5f91a31d503a80f2c8dc7f1fecda5593da44f1
Matt Carlson <matthewthomascarlson@gmail.com>
2016-08-05 09:51:09 -0700

    edit getRepos method in user.js to make links out of repo titles

--- js/user.js
+++ js/user.js
@@ -5,25 +5,20 @@ function User() {
 
 User.prototype.getRepos = function(username) {
   $.get('https://api.github.com/users/' + username + '/repos?page=1&per_page=100&access_token=' + apiKey).then(function(response){
-    // console.log(response);
+    console.log(response);
     var responseHtml = "";
     var repos = response;
     for (var i = 0; i < repos.length; i++) {
-      // console.log(response[i].name);
-      // console.log(response[i].description);
-      created = moment(repos[i].pushed_at);
-      responseHtml += "<h3>" + repos[i].name + "</h3><br>";
-      responseHtml += "<h5>Created on: " + (created.month() + 1) + "/" + (created.date() + 1) + "/" + created.year() + "</h5><br>";
+      var created = moment(repos[i].pushed_at);
+      responseHtml += '<h3><a href="' + repos[i].html_url + '">' + repos[i].name + '</a></h3><br>';
+      responseHtml += "<h5>" + repos[i].language + "</h5><br>";
+      responseHtml += "<h5>Created on: " + (created.month() + 1) + "/" + created.date() + "/" + created.year() + "</h5><br>";
       if (repos[i].description === null) {
         responseHtml += "<hr>";
       } else {
         responseHtml += "<h5>" + repos[i].description + "</h5><hr>";
       }
-
-      // console.log("Created on: " + (created.month() + 1) + "/" + (created.date() + 1) + "/" + created.year());
-      // responseHtml += "Created on: " +
     }
-    // console.log(response);
     $(".showInfo").html(responseHtml);
   }).fail(function(error){
     console.log(error.responseJSON.message);
